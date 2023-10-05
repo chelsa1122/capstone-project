8815e82787c08e78bf3814751201358835442e66
Stuti Jayswal <jayswalstuti9@gmail.com>
2023-10-05 03:55:26 -0400

    improved proper design

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,66 +1,91 @@
-import * as React from 'react';
-import Container from '@mui/material/Container';
-import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import Link from 'next/link';
-import Navbar from '@/components/Navbar';
+import { Box } from "@mui/material";
+import Button from "@mui/material/Button";
+import Paper from "@mui/material/Paper";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import Checkbox from "@mui/material/Checkbox";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Link from "@mui/material/Link";
+import { Grid } from "@mui/material";
 
 const LoginPage = () => {
   const handleLogin = () => {
     // Implement your login logic here
-    console.log('Login button clicked');
+    console.log("Login button clicked");
   };
 
   return (
-    <>
-    <Navbar />
-    <Container
+    <Box
       sx={{
-        height: '100vh',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
+        display: "flex",
+        alignItems: "stretch", // Stretch both sides vertically
+        height: "100vh",
+        background: "#FFC900", // Set the background color to yellow
       }}
     >
-      <Paper elevation={3}
-        sx={{
-          padding: 3,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          width: '300px',
+      {/* Left Side - Yellow */}
+      <div
+        style={{
+          flex: "0 0 30%", // Take up 30% of the viewport width
         }}
       >
-        <Typography variant="h5" gutterBottom >
+        {/* You can add content here if needed */}
+      </div>
+
+      {/* Right Side - White with Login Form */}
+      <Paper
+        elevation={3}
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center", // Center horizontally
+          flex: "0 0 70%", // Take up 70% of the viewport width
+          borderRadius: "50px",
+          backgroundColor: "white", // Set the background color to white
+        }}
+      >
+        <div style={{ flex: 1 }} /> {/* Spacer to push content to the center */}
+        <Typography variant="h5" gutterBottom>
           Login
         </Typography>
         <TextField
-          sx={{ marginBottom: 2 }}
-          label="Username"
+          sx={{ marginBottom: 1 }}
+          label="Email"
           variant="outlined"
-          fullWidth
+          style={{ width: "50%" }}
         />
         <TextField
-          sx={{ marginBottom: 2 }}
+          sx={{ marginBottom: 1 }}
           label="Password"
           type="password"
           variant="outlined"
-          fullWidth
+          style={{ width: "50%" }}
         />
+        <Grid container sx={{ marginTop: 1 }}>
+          <Grid item xs={6} sx={{ textAlign: "left" }}>
+            <FormControlLabel
+              control={<Checkbox />}
+              label="Remember me"
+              sx={{ paddingLeft: "20px" }}
+            />
+          </Grid>
+          <Grid item xs={6} sx={{ textAlign: "right" }}>
+            <Link href="#" sx={{ paddingRight: "20px" }}>
+              Forgot Password?
+            </Link>
+          </Grid>
+        </Grid>
         <Button
-          sx={{ marginTop: 2 }}
+          sx={{ marginTop: 1, width: "50%" }}
           variant="contained"
           color="primary"
-          fullWidth
           onClick={handleLogin}
         >
           Log In
         </Button>
+        <div style={{ flex: 1 }} /> {/* Spacer to push content to the center */}
       </Paper>
-    </Container>
-    </>
+    </Box>
   );
 };
 
