d6ffaa6d034649afe843796f6240bb29789c726a
Tanmayi-p <tpatel8916@conestogac.on.ca>
2023-11-16 11:36:13 -0500

    API changes for services

--- src/controllers/servicesController.js
+++ src/controllers/servicesController.js
@@ -6,14 +6,34 @@ const router = express.Router();
 // Function to get services available for pets in a specific location
 const getServicesByLocation = (req, res) => {
   // Extract location from request parameters or query string
-  const location = req.params.location || req.query.location;
+  const service_location = req.params.location || req.query.location;
+  const pet = req.params.location || req.query.location;
+  const service = req.params.location || req.query.location;
 
-  if (!location) {
-    return res.status(400).json({ error: 'Location is required' });
-  }
+  // if (!location) {
+  //   return res.status(400).json({ error: 'Location is required' });
+  // }
 
   // Query to get services based on the location
-  const query = 'SELECT * FROM services WHERE location = ?';
+  const query = 'SELECT * FROM services WHERE';
+
+  if (location && pet && service) {
+    query += ' service_location = ? AND pet = ? AND service = ?';
+  } else if (location && pet) {
+    query += ' service_location = ? AND pet = ?';
+  } else if (location && service) {
+    query += ' service_location = ? AND service = ?';
+  } else if (pet && service) {
+    query += ' pet = ? AND service = ?';
+  } else if (location) {
+    query += ' service_location = ?';
+  } else if (pet) {
+    query += ' pet = ?';
+  } else if (service) {
+    query += ' service = ?';
+  } else {
+    query ='SELECT * FROM services';
+  }
 
   db.query(query, [location], (error, results) => {
     if (error) {
